44cd010e80bb255528827c8d35d8fabd6dbd07e1
datacouch <jon@walker.ninja>
2018-03-01 15:51:45 -0800

    ' ' for the win

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,3 @@
 public class HelloWorld {
-public static void main (String [] args) { System.out.println("Hello Worlds");
+public static void main (String [] args) { System.out.println("Hello World ");
 } }
